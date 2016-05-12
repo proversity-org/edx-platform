45c0bbfe3e51f707cec6ec47c322401f049c1b74
dominic <dominic@hubblestudios.com>
2016-05-12 14:51:03 +0200

    respect visibility status of discussion panel

--- lms/djangoapps/django_comment_client/utils.py
+++ lms/djangoapps/django_comment_client/utils.py
@@ -13,8 +13,12 @@ from django.utils.timezone import UTC
 from opaque_keys.edx.keys import CourseKey
 from opaque_keys.edx.locations import i4xEncoder
 
+<<<<<<< HEAD
 import pystache_custom as pystache
 from courseware import courses
+=======
+from courseware import courses, tabs
+>>>>>>> respect visibility status of discussion panel
 from courseware.access import has_access
 from django_comment_client.constants import TYPE_ENTRY, TYPE_SUBCATEGORY
 from django_comment_client.permissions import check_permissions_by_view, get_team, has_permission
@@ -1022,6 +1026,9 @@ def is_discussion_enabled(course_id):
     """
     Return True if discussions are enabled; else False
     """
+    course = courses.get_course_by_id(course_id)
+    if not tabs.ExternalDiscussionCourseTab.is_enabled(course):
+        return False
     return settings.FEATURES.get('ENABLE_DISCUSSION_SERVICE')
 
 
