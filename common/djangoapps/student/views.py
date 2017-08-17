2a2bef65af57cf42b60984d86c663a3f47c495c4
Jose Antonio Gonzalez <antonio@proversity.org>
2017-08-17 10:31:39 +0200

    inject csrf token to form

--- common/djangoapps/student/views.py
+++ common/djangoapps/student/views.py
@@ -1850,6 +1850,7 @@ def authenticate_to_linkedin_using_msdk(access_token, user):
     headers = {'x-li-src': 'msdk', 'Authorization': 'Bearer ' + access_token}
     url = 'https://api.linkedin.com/v1/people/~%s' % fields
     r = requests.get(url, params=params, headers=headers)
+<<<<<<< HEAD
     log.info('===================================')
     log.info('authenticate_to_linkedin_using_msdk_in_registration')
     log.info(r.json())
@@ -1859,6 +1860,8 @@ def authenticate_to_linkedin_using_msdk(access_token, user):
     return user
 >>>>>>> sso to linkedin for mobile
 =======
+=======
+>>>>>>> inject csrf token to form
     if r.status_code == 200:
         return user
     else:
