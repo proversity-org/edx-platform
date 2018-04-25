648d2beadc1bb9612e73155b0e79bcfcfe052c83
Brian Mesick <bmesick@edx.org>
2018-04-25 10:39:26 -0400

    Merge pull request #17920 from edx/bmedx/fix_retirement_apis
    Update retirement APIs to match spec

--- openedx/core/djangoapps/user_api/models.py
+++ openedx/core/djangoapps/user_api/models.py
@@ -122,3 +122,159 @@ class UserOrgTag(TimeStampedModel):
 
     class Meta(object):
         unique_together = ("user", "org", "key")
+
+
+class RetirementState(models.Model):
+    """
+    Stores the list and ordering of the steps of retirement, this should almost never change
+    as updating it can break the retirement process of users already in the queue.
+    """
+    state_name = models.CharField(max_length=30, unique=True)
+    state_execution_order = models.SmallIntegerField(unique=True)
+    is_dead_end_state = models.BooleanField(default=False, db_index=True)
+    required = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return u'{} (step {})'.format(self.state_name, self.state_execution_order)
+
+    class Meta(object):
+        ordering = ('state_execution_order',)
+
+    @classmethod
+    def get_dead_end_states(cls):
+        return cls.objects.filter(is_dead_end_state=True)
+
+    @classmethod
+    def get_dead_end_state_names_list(cls):
+        return cls.objects.filter(is_dead_end_state=True).values_list('state_name', flat=True)
+
+    @classmethod
+    def get_state_names_list(cls):
+        return cls.objects.all().values_list('state_name', flat=True)
+
+
+@receiver(pre_delete, sender=RetirementState)
+def retirementstate_pre_delete_callback(_, **kwargs):
+    """
+    Event changes to user preferences.
+    """
+    state = kwargs["instance"]
+    if state.required:
+        raise Exception('Required RetirementStates cannot be deleted.')
+
+
+class UserRetirementStatus(TimeStampedModel):
+    """
+    Tracks the progress of a user's retirement request
+    """
+    user = models.OneToOneField(User)
+    original_username = models.CharField(max_length=150, db_index=True)
+    original_email = models.EmailField(db_index=True)
+    original_name = models.CharField(max_length=255, blank=True, db_index=True)
+    retired_username = models.CharField(max_length=150, db_index=True)
+    retired_email = models.EmailField(db_index=True)
+    current_state = models.ForeignKey(RetirementState, related_name='current_state')
+    last_state = models.ForeignKey(RetirementState, blank=True, related_name='last_state')
+    responses = models.TextField()
+
+    class Meta(object):
+        verbose_name = 'User Retirement Status'
+        verbose_name_plural = 'User Retirement Statuses'
+
+    def _validate_state_update(self, new_state):
+        """
+        Confirm that the state move that's trying to be made is allowed
+        """
+        dead_end_states = list(RetirementState.get_dead_end_state_names_list())
+        states = list(RetirementState.get_state_names_list())
+        if self.current_state in dead_end_states:
+            raise RetirementStateError('RetirementStatus: Unable to move user from {}'.format(self.current_state))
+
+        try:
+            new_state_index = states.index(new_state)
+            if new_state_index <= states.index(self.current_state.state_name):
+                raise ValueError()
+        except ValueError:
+            err = '{} does not exist or is an eariler state than current state {}'.format(new_state, self.current_state)
+            raise RetirementStateError(err)
+
+    def _validate_update_data(self, data):
+        """
+        Confirm that the data passed in is properly formatted
+        """
+        required_keys = ('username', 'new_state', 'response')
+
+        for required_key in required_keys:
+            if required_key not in data:
+                raise RetirementStateError('RetirementStatus: Required key {} missing from update'.format(required_key))
+
+        for key in data:
+            if key not in required_keys:
+                raise RetirementStateError('RetirementStatus: Unknown key {} in update'.format(key))
+
+    @classmethod
+    def create_retirement(cls, user):
+        """
+        Creates a UserRetirementStatus for the given user, in the correct initial state. Will
+        fail if the user already has a UserRetirementStatus row or if states are not yet populated.
+        """
+        try:
+            pending = RetirementState.objects.all().order_by('state_execution_order')[0]
+        except IndexError:
+            raise RetirementStateError('Default state does not exist! Populate retirement states to retire users.')
+
+        if cls.objects.filter(user=user).exists():
+            raise RetirementStateError('User {} already has a retirement row!'.format(user))
+
+        retired_username = get_retired_username_by_username(user.username)
+        retired_email = get_retired_email_by_email(user.email)
+
+        return cls.objects.create(
+            user=user,
+            original_username=user.username,
+            original_email=user.email,
+            original_name=user.profile.name,
+            retired_username=retired_username,
+            retired_email=retired_email,
+            current_state=pending,
+            last_state=pending,
+            responses='Created in state {} by create_retirement'.format(pending)
+        )
+
+    def update_state(self, update):
+        """
+        Perform the necessary checks for a state change and update the state and response if passed
+        or throw a RetirementStateError with a useful error message
+        """
+        self._validate_update_data(update)
+        self._validate_state_update(update['new_state'])
+
+        old_state = self.current_state
+        self.current_state = RetirementState.objects.get(state_name=update['new_state'])
+        self.last_state = old_state
+        self.responses += "\n Moved from {} to {}:\n{}\n".format(old_state, self.current_state, update['response'])
+        self.save()
+
+    @classmethod
+    def get_retirement_for_retirement_action(cls, username):
+        """
+        Convenience method to get a UseRetirementStatus for a particular user with some checking
+        to make sure they're in a state that is acceptable to be acted upon. The user should be
+        in a "working state" (not a dead end state, PENDING, or *_COMPLETE). This should help
+        a bit with situations like the retirement driver accidentally trying to act upon the
+        same user twice at the same time, or trying to take action on an errored user.
+
+        Can raise UserRetirementStatus.DoesNotExist or RetirementStateError, otherwise should
+        return a UserRetirementStatus
+        """
+        retirement = cls.objects.get(original_username=username)
+        state = retirement.current_state
+
+        if state.required or state.state_name.endswith('_COMPLETE'):
+            raise RetirementStateError('{} is in {}, not a valid state to perform retirement '
+                                       'actions on.'.format(retirement, state.state_name))
+
+        return retirement
+
+    def __unicode__(self):
+        return u'User: {} State: {} Last Updated: {}'.format(self.user.id, self.current_state, self.modified)
