6e353ce7c421b5ac55bd31271e4b61a8ce835c2f
nicovanniekerk <nico@proversity.org>
2018-05-02 09:43:20 +0200

    comments and cleanup

--- openedx/core/djangoapps/theming/custom_memcache_switcher.py
+++ openedx/core/djangoapps/theming/custom_memcache_switcher.py
@@ -15,52 +15,50 @@ from django.core.cache import (
 )
 
 import logging
-log = logging.getLogger("CUSTOM CACHE")
+log = logging.getLogger(__name__)
 
 class CustomCachedFilesMixin(HashedFilesMixin):
 
     def __init__(self, *args, **kwargs):
         super(CustomCachedFilesMixin, self).__init__(*args, **kwargs)
 
-
+        # Setup the version holder cache.
         cache_version_holder = memcache.Client(['localhost:11211'], debug=0)
         if not cache_version_holder.get('build_version'):
             cache_version_holder.add('build_version', 'staticfiles_2') 
         if not cache_version_holder.get('run_version'):
-            cache_version_holder.add('run_version', 'staticfiles')
+            cache_version_holder.add('run_version', 'staticfiles_1')
         if not cache_version_holder.get('has_lms_ran'):
             cache_version_holder.add('has_lms_ran', 'no')
         if not cache_version_holder.get('has_cms_ran'):
             cache_version_holder.add('has_cms_ran', 'no')
 
 
-        print("CUSTOM STORAGE GUY")
         try:
 
             if self._is_build_calling(inspect.stack(), cache_version_holder):
                 self.hashed_files = _MappingCache(caches[cache_version_holder.get('build_version')])
-                log.error("Build is calling setting hashed files to {}".format(cache_version_holder.get('build_version')))
                 if self._has_both_ran(cache_version_holder):
                     cache_version_holder.set('run_version', cache_version_holder.get('build_version'))
-                    log.error("Setting the run_version = build_version")
                     if cache_version_holder.get('build_version') == 'staticfiles_2':
-                        log.error("updating build_version to staticfiles")
-                        cache_version_holder.set('build_version', 'staticfiles')
+                        cache_version_holder.set('build_version', 'staticfiles_1')
                     else:
-                        log.error("updating build_version to staticfiles_2")
                         cache_version_holder.set('build_version', 'staticfiles_2')
             else:
-                log.error("run called, starting hashed files version {}".format(cache_version_holder.get('run_version')))
                 self.hashed_files = _MappingCache(caches[cache_version_holder.get('run_version')])
         except InvalidCacheBackendError:
             # Use the default backend
             self.hashed_files = _MappingCache(default_cache)
+            log.error("SInvalid backends provided, check the CACHES setting in the *.env.json files. staticfiles_1 and staticfiles_2")
 
     def hash_key(self, name):
         key = hashlib.md5(force_bytes(self.clean_name(name))).hexdigest()
         return 'staticfiles:%s' % key
 
     def _is_build_calling(self, stack,cache_version_holder):
+        """
+        Utility function to inspect the stack and return if the collectstatic command was issued in the call stack
+        """
         column_len = len(stack[0])
         row_len = len(stack)
         calling = False
@@ -73,11 +71,13 @@ class CustomCachedFilesMixin(HashedFilesMixin):
         return calling
 
     def _has_both_ran(self, cache_version_holder):
+        """
+        Utility function to detect which of the two systems is being called and set that they have been build as assets.
+        If both was built the values will be reset and we start a fresh.
+        """
         if settings.ROOT_URLCONF == 'lms.urls':
             cache_version_holder.set('has_lms_ran', 'yes')
-            print("AAAAA LMS CALLING")
         if settings.ROOT_URLCONF == 'cms.urls':
-            print("AAAAA CMS CALLING")
             cache_version_holder.set('has_cms_ran', 'yes')
         if cache_version_holder.get('has_lms_ran') == 'yes' and cache_version_holder.get('has_lms_ran') == 'yes':
             print("BOTH HAS RAN")
