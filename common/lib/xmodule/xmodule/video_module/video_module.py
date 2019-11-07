0f2ddd8f4aab2f36599ab3ef08bf61023da81029
Squirrel18 <daniel.quiroga@edunext.co>
2019-11-07 15:30:41 -0500

    Add show_captions context to the video_module.

--- common/lib/xmodule/xmodule/video_module/video_module.py
+++ common/lib/xmodule/xmodule/video_module/video_module.py
@@ -1153,4 +1153,5 @@ class VideoDescriptor(VideoFields, VideoTranscriptsMixin, VideoStudioViewHandler
             "transcripts": transcripts,
             "encoded_videos": encoded_videos,
             "all_sources": all_sources,
+            "show_captions": self.show_captions,
         }
